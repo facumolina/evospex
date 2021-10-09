6fdf34c9de57640d1f868f881417414f5d51d3d3
Facundo Molina <facu.molina7@gmail.com>
2021-10-09 18:02:00 -0300

    Adding more ExprNames

--- src/main/java/evospex/expression/ExprName.java
+++ src/main/java/evospex/expression/ExprName.java
@@ -10,5 +10,10 @@ public class ExprName {
   public static final String THIS = "this"; // Name for expression this
   public static final String THIS_PRE = "this_pre"; // Name for expression old(this)
   public static final String NULL = "null"; // Name for expression null
+  public static final String TRUE = "true"; // Name for expression true
+  public static final String FALSE = "false"; // Name for expression false
+  public static final String RESULT = "result"; // Name for the result expression (result of the method)
+  public static final String ZERO = "0"; // Name for the result expression 0
+  public static final String ONE = "1"; // Name for the result expression 1
 
 }
