ad5883c4f10e99b05dd6c6064c03ebd94dba5ea8
Facundo Molina <facu.molina7@gmail.com>
2021-10-29 10:02:27 -0300

    Adding class with utils for Java classes

---
+++ src/main/java/utils/JavaClassesUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+/**
+ * This class provides utilities for handling Java classes
+ *
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar></>
+ */
+public class JavaClassesUtils {
+
+  /**
+   * Check if the given class is a numberic class
+   * @param cl is the given class
+   * @return true iff cl is a numeric class
+   */
+  public static boolean isNumber(Class<?> cl) {
+    if (cl == null) throw new IllegalArgumentException();
+    return Number.class.isAssignableFrom(cl);
+  }
+
+}
