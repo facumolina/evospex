24a4e73e9a9f3b750b10388931c3c9fd493ca769
Facundo Molina <facu.molina7@gmail.com>
2021-10-30 19:37:00 -0300

    Incorporating helper methods in class Expr

--- src/main/java/evospex/expression/Expr.java
+++ src/main/java/evospex/expression/Expr.java
@@ -4,6 +4,8 @@ import evospex.expression.ExprGrammarParser.ExprContext;
 import evospex.expression.symbol.ExprName;
 import evospex.expression.symbol.ExprOperator;
 
+import java.util.List;
+
 /**
  * Expr class captures an expression, which is composed of an ExprContext (the actual expression)
  * and a class, which represent the expression type.
@@ -14,6 +16,7 @@ public class Expr {
 
   private final ExprContext exprContext; // Actual expression
   private final Class<?> type; // The expression type
+  private Class<?> quantificationOverType; // Class of elements in a quantified set
 
   /**
    * Constructor
@@ -23,6 +26,7 @@ public class Expr {
       throw new IllegalArgumentException("Neither the expression nor the class can be null");
     exprContext = exprCtx;
     type = c;
+    quantificationOverType = null;
   }
 
   /**
@@ -44,6 +48,25 @@ public class Expr {
    */
   public boolean isQuantified() { return exprContext.qt_expr() != null; }
 
+  /**
+   * Returns the class of the elements of the set being quantified
+   */
+  public Class<?> classOfElemsInSet() { return quantificationOverType; };
+
+  /**
+   * Set the class of the elements of the set being quantified
+   * @param cl is the class of the elements
+   */
+  public void setClassOfElemsInSet(Class<?> cl) {
+    if (cl == null) throw new IllegalArgumentException();
+    quantificationOverType = cl;
+  }
+
+  /**
+   * Returns true iff the current expression is about a pre state
+   */
+  public boolean isPreState() { return toString().contains(ExprName.THIS_PRE); }
+
   @Override
   public String toString() {
     String str =  exprContext.getText();
@@ -55,6 +78,14 @@ public class Expr {
     }
     if (str.contains(ExprName.QT_VAR + ExprOperator.IN + ExprName.QT_VAR))
       str = str.replace(ExprName.QT_VAR + ExprOperator.IN + ExprName.QT_VAR, ExprName.QT_VAR + " " + ExprOperator.IN + " " + ExprName.QT_VAR);
+
+    ExprGrammarParser.Compare_opContext cmp_op = exprContext.compare_op();
+    if (cmp_op != null && ExprOperator.IN.equals(cmp_op.getText())) {
+      // The operator is in, so make this string more clear
+      List<ExprContext> exprs = exprContext.expr();
+      str = exprs.get(0).getText() + " " + ExprOperator.IN + " " + exprs.get(1).getText();
+    }
+
     return str;
   }
 
