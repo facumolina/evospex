efc7e4ed3d321bfdde63ac28cc070fcb1882a86d
Facundo Molina <facu.molina7@gmail.com>
2022-02-21 08:56:59 -0300

    dynalloy rfm library is no longer a dependency

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mvn install:install-file -Dfile=lib/dynalloy.jar -DgroupId=rfm.dynalloy -DartifactId=dynalloy -Dversion=1.0.1 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/canonicalizer.jar -DgroupId=dc.unrc -DartifactId=canonicalizer -Dversion=1.0.1 -Dpackaging=jar
