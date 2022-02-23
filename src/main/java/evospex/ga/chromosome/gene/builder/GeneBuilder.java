63689e8965ac4c87828b0bf4fba2d58b859b1602
Facundo Molina <facu.molina7@gmail.com>
2022-02-23 13:19:48 -0300

    Abstract class to implement gene builders

---
+++ src/main/java/evospex/ga/chromosome/gene/builder/GeneBuilder.java
@@ -0,0 +1,37 @@
+package evospex.ga.chromosome.gene.builder;
+
+import evospex.EvoSpexParameters;
+import org.jgap.Configuration;
+import org.jgap.Gene;
+import org.jgap.InvalidConfigurationException;
+import utils.TargetInformation;
+
+import java.util.List;
+
+/**
+ * This class represents a Gene Builder, which provides mechanism to create specific genes to be part
+ * of the chromosomes.
+ *
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
+public abstract class GeneBuilder {
+
+  protected Configuration conf;
+  protected final TargetInformation targetInfo;
+  protected EvoSpexParameters parameters;
+
+  /**
+   * Constructor
+   */
+  public GeneBuilder(Configuration conf, TargetInformation info, EvoSpexParameters params) {
+    this.conf = conf;
+    targetInfo = info;
+    parameters = params;
+  }
+
+  /**
+   * Returns a list of genes to be part of the chromosomes
+   */
+  public abstract List<Gene> build() throws InvalidConfigurationException;
+
+}
