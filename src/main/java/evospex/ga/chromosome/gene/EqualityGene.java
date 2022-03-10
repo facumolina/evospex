72731fb7e1b1658561bcfd85aa2bb9ec8a0412b9
Facundo Molina <facu.molina7@gmail.com>
2022-03-10 13:48:13 -0300

    Updating comments

--- src/main/java/evospex/ga/chromosome/gene/EqualityGene.java
+++ src/main/java/evospex/ga/chromosome/gene/EqualityGene.java
@@ -13,6 +13,12 @@ import utils.TargetInformation;
 
 import java.util.List;
 
+/**
+ * This class represents an equality gene. A equality gene is a gene which value is an expression
+ * of the form e (= | !=) f, where e and f are non primitive expressions of the same type.
+ *
+ * @author Facundo Molina <fmolina@dc.exa.unrc.edu.ar>
+ */
 public class EqualityGene extends ExprGene {
 
   public EqualityGene(Configuration a_conf, ExprGeneValue value, TargetInformation info) throws InvalidConfigurationException {
@@ -59,30 +65,6 @@ public class EqualityGene extends ExprGene {
   }
 
   @Override
-  public void applyMutation(int i, double v) {
-    updatePreviousExpression(value.clone());
-    String mutationToApply = getSomeApplicableMutation();
-    Expr expr = value.getExpression();
-
-    // Check the expression is adequate
-    ExprGrammarParser.Compare_opContext cmp_op = expr.exprCtx().compare_op();
-    if (cmp_op==null)
-      throw new IllegalStateException("The expression "+expr+" should be a comparison with = or !=");
-    List<ExprGrammarParser.ExprContext> expressions = expr.exprCtx().expr();
-    if (expressions.size() != 2)
-      throw new IllegalStateException("The expression "+expr+" should only have two expressions");
-
-    if (mutationToApply.equals(ExprGeneMutations.NEGATE)) {
-      ExprGrammarParser.ExprContext left = expressions.get(0);
-      ExprGrammarParser.ExprContext right = expressions.get(1);
-      String new_op = cmp_op.getText().equals(ExprOperator.EQ)?ExprOperator.NOT_EQ:ExprOperator.EQ;
-      Expr mutatedExpr = ExprBuilder.toExpr(left.getText() + " " + new_op + " " + right.getText(), Boolean.class);
-      value.setExpression(mutatedExpr, false);
-    } else if (mutationToApply.equals(ExprGeneMutations.TO_TRUE)){
-      ExprGeneMutationHelper.toTrue(value);
-    } else {
-      throw new UnsupportedOperationException("Unsupported mutation: " + mutationToApply);
-    }
-  }
+  public void applyMutation(int i, double v) { }
 
 }
