d9119f83323093f7b3b85361df4e020725b0f0e2
Facundo Molina <facu.molina7@gmail.com>
2022-06-22 08:46:06 -0300

    Update EvoSpex script

--- evospex.sh
+++ evospex.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+# This script allows to perform the postcondition assertion inference phase by running EvoSpex.
 root_dir=$PWD;
+subject_cp=$1
+target_class_fqn=$2
+method_states=$3
 
-target_class_fqn=$1
-base_folder=$2;
-
-cd $base_folder;
+# Count arguments and outputs
+cd $method_states;
 num_args="$( ls -l in*.xml | wc -l )";
 num_outputs="$( ls -l out*.xml | wc -l )";
 cd $root_dir;
-java -jar target/evospex.jar $target_class_fqn $2 $num_args $num_outputs $3 $4 $5 $6 $7 $8
+
+# Run EvoSpex
+java -cp $subject_cp -jar target/evospex.jar $target_class_fqn $method_states $num_args $num_outputs $3 $4 $5 $6 $7 $8
