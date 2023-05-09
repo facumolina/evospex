0828e5bbeb5d6ba6750f90ed3dd1b84205446353
Facundo Molina <facu.molina7@gmail.com>
2023-05-09 18:47:22 +0200

    Ensuring that all inputs are different

--- src/test/java/casestudies/motivation/AvlTreeListTest.java
+++ src/test/java/casestudies/motivation/AvlTreeListTest.java
@@ -13,16 +13,16 @@ public class AvlTreeListTest {
   @Test
   public void testAdd2() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
-    list.add(1);
     list.add(2);
+    list.add(3);
   }
 
   @Test
   public void testAdd3() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
-    list.add(1);
-    list.add(2);
-    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
   }
 
 }
