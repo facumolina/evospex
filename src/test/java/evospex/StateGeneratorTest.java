1351f0b20a615681387c333b4836e214c510915a
Facundo Molina <facu.molina7@gmail.com>
2023-05-10 07:54:25 +0200

    StateGeneratorTest assertions and improved

--- src/test/java/evospex/StateGeneratorTest.java
+++ src/test/java/evospex/StateGeneratorTest.java
@@ -3,19 +3,20 @@ package evospex;
 import evospex.state.StateSerializer;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 public class StateGeneratorTest {
 
   @Test
-  public void test() {
+  public void testCollectedObjects() {
     StateGenerator stateGenerator = new StateGenerator("casestudies.motivation.AvlTreeListTest");
     try {
       stateGenerator.generateStates();
       List<Object> inputs = StateSerializer.inputs;
       List<Object> outputs = StateSerializer.outputs;
-      System.out.println("Inputs: " + inputs);
-      System.out.println("Outputs: " + outputs);
+      assertEquals(6, inputs.size());
+      assertEquals(6, outputs.size());
     } catch (Exception e) {
       assert false;
     }
