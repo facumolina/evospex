a2cf042bee0c080f2f6e32e1f898f29d7c6658d0
Facundo Molina <facu.molina7@gmail.com>
2023-05-10 07:53:50 +0200

    State serializer is serialyzing before saving

--- src/main/java/evospex/state/StateSerializer.java
+++ src/main/java/evospex/state/StateSerializer.java
@@ -1,23 +1,35 @@
 package evospex.state;
 
+import com.thoughtworks.xstream.XStream;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * StateSerializer class: serializes inputs and outputs of a test case
+ * @author Facundo Molina
+ */
 public class StateSerializer {
 
+  // List of inputs and outputs
   public static List<Object> inputs = new ArrayList<>();
   public static List<Object> outputs = new ArrayList<>();
+
+  // Actual serializer
+  private static XStream xstream = new XStream();
 
   public static void serializeInput(Object input) {
     System.out.println("Serializing input");
     System.out.println(input);
-    inputs.add(input);
+    Object toPreserve = xstream.toXML(input);
+    inputs.add(toPreserve);
   }
 
   public static void serializeOutput(Object output) {
     System.out.println("Serializing output");
     System.out.println(output);
-    outputs.add(output);
+    Object toPreserve = xstream.toXML(output);
+    outputs.add(toPreserve);
   }
 
 }
