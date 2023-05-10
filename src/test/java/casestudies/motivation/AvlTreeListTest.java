bf6f853123a90c7b865d80772fab7c31d6f3efef
Facundo Molina <facu.molina7@gmail.com>
2023-05-10 18:04:51 +0200

    Update input analysis test

--- src/test/java/casestudies/motivation/AvlTreeListTest.java
+++ src/test/java/casestudies/motivation/AvlTreeListTest.java
@@ -7,22 +7,28 @@ public class AvlTreeListTest {
   @Test
   public void testAdd() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
-    list.add(1);
+    boolean b1 = list.add(1);
+    assert b1;
   }
 
   @Test
   public void testAdd2() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
-    list.add(2);
-    list.add(3);
+    boolean b2 = list.add(2);
+    boolean b3 = list.add(3);
+    assert b2;
+    assert b3;
   }
 
   @Test
   public void testAdd3() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
-    list.add(4);
-    list.add(5);
-    list.add(6);
+    boolean b4 = list.add(4);
+    boolean b5 = list.add(5);
+    boolean b6 = list.add(6);
+    assert b4;
+    assert b5;
+    assert b6;
   }
 
 }
