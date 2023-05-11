d172c5cb6db30523db5bcaf605bbcb15b785d734
Facundo Molina <facu.molina7@gmail.com>
2023-05-11 15:14:05 +0200

    First version actually preserving the serializing states in files

--- src/main/java/evospex/state/StateSerializer.java
+++ src/main/java/evospex/state/StateSerializer.java
@@ -1,9 +1,12 @@
 package evospex.state;
 
 import com.thoughtworks.xstream.XStream;
-import soot.SootClass;
 import soot.SootMethod;
 
+import java.io.File;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,8 @@ import java.util.List;
  */
 public class StateSerializer {
 
+  private static String targetClassName;
+  private static String targetMethodSignature;
 
   // List of inputs and outputs
   public static List<Object> inputsThis = new ArrayList<>();
@@ -26,7 +31,12 @@ public class StateSerializer {
   private static int arguments = 0;
 
   // Actual serializer
-  private static XStream xstream = new XStream();
+  private static final XStream xstream = new XStream();
+
+  // Serialization
+  private static final String BASE_OUTPUT_FOLDER = "states";
+  private static final String INPUTS_FILE_BASE_NAME = "in";
+  private static final String OUTPUTS_FILE_BASE_NAME = "out";
 
   /**
    * Setup the serializer from the target method.
@@ -34,6 +44,8 @@ public class StateSerializer {
    */
   public static void setup(SootMethod targetMethod) {
     arguments = targetMethod.getParameterCount();
+    targetClassName = targetMethod.getDeclaringClass().getName();
+    targetMethodSignature = targetMethod.getSignature();
   }
 
   /**
@@ -56,7 +68,7 @@ public class StateSerializer {
     System.out.println("--> Serializing input");
     System.out.println("position: " + position);
     System.out.println("object: " + input);
-    Object toPreserve = xstream.toXML(input);
+    Object toPreserve = xstream.fromXML(xstream.toXML(input));
     if (position == 0) {
       inputsThis.add(toPreserve);
     } else {
@@ -76,7 +88,7 @@ public class StateSerializer {
     System.out.println("--> Serializing output");
     System.out.println("position: " + position);
     System.out.println("object: "+output);
-    Object toPreserve = xstream.toXML(output);
+    Object toPreserve = xstream.fromXML(xstream.toXML(output));
     if (position == 0) {
       outputsThis.add(toPreserve);
     } else {
@@ -90,4 +102,64 @@ public class StateSerializer {
       }
     }
   }
+
+  /**
+   * Saves the serialization to a file.
+   */
+  public static void saveSerialization() {
+    System.out.println("--> Saving serialization");
+    String folder = formatFolderName(targetMethodSignature);
+    System.out.println("> output folder: " + folder);
+    // Save inputs objects
+    String inputsThisFile = folder + "/" + INPUTS_FILE_BASE_NAME + "0.xml";
+    saveObjects(inputsThisFile, inputsThis);
+    System.out.println("> saved: "+inputsThisFile);
+    // Save the input arguments objects (if any)
+    if (inputsArgs.size() > 0) {
+      for (int i = 0; i < inputsArgs.size(); i++) {
+        String inputsArgsFile = folder + "/" + INPUTS_FILE_BASE_NAME + (i + 1) + ".xml";
+        saveObjects(inputsArgsFile, inputsArgs.get(i));
+        System.out.println("> saved: "+inputsArgsFile);
+      }
+    }
+    // Save outputs objects
+    String outputsThisFile = folder + "/" + OUTPUTS_FILE_BASE_NAME + "0.xml";
+    saveObjects(outputsThisFile, outputsThis);
+    System.out.println("> saved: "+outputsThisFile);
+    // Save the output arguments objects (if any)
+    if (outputsArgs.size() > 0) {
+      for (int i = 0; i < outputsArgs.size(); i++) {
+        String outputsArgsFile = folder + "/" + OUTPUTS_FILE_BASE_NAME + (i + 1) + ".xml";
+        saveObjects(outputsArgsFile, outputsArgs.get(i));
+        System.out.println("> saved: "+outputsArgsFile);
+      }
+    }
+  }
+
+  /**
+   * Save the objects from the given list in the given file name
+   */
+  private static void saveObjects(String fileName, List<Object> objects) {
+    try {
+      File f = new File(fileName);
+      f.getParentFile().mkdirs();
+      ObjectOutputStream oos = xstream.createObjectOutputStream(Files.newOutputStream(Paths.get(fileName)));
+      for (Object o : objects) {
+        oos.writeObject(o);
+      }
+      oos.close();
+    } catch (Exception e) {
+      System.out.println("Error saving " + fileName);
+    }
+  }
+  /**
+   * Format folder name for a target method.
+   * @param targetMethodSignature the target method signature.
+   */
+  public static String formatFolderName(String targetMethodSignature) {
+    String formattedFolderName = targetMethodSignature.replace("<", "").replace(">", "").replace(":", "").replace(" ", "_");
+    formattedFolderName = formattedFolderName.replace(targetClassName,"");
+    return BASE_OUTPUT_FOLDER + "/" + targetClassName + "/" + formattedFolderName;
+  }
+
 }
