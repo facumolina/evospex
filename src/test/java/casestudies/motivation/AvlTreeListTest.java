2937dde03b1853af7d3839aeecd01095c79f9dae
Facundo Molina <facu.molina7@gmail.com>
2023-05-11 09:24:26 +0200

    Target AvlTreeListTest includes calls to size

--- src/test/java/casestudies/motivation/AvlTreeListTest.java
+++ src/test/java/casestudies/motivation/AvlTreeListTest.java
@@ -2,6 +2,7 @@ package casestudies.motivation;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AvlTreeListTest {
@@ -10,7 +11,9 @@ public class AvlTreeListTest {
   public void testAdd() {
     AvlTreeList<Integer> list = new AvlTreeList<>();
     boolean b1 = list.add(1);
+    int size = list.size();
     assertTrue(b1);
+    assertEquals(1, size);
   }
 
   @Test
@@ -18,8 +21,10 @@ public class AvlTreeListTest {
     AvlTreeList<Integer> list = new AvlTreeList<>();
     boolean b2 = list.add(2);
     boolean b3 = list.add(3);
+    int size = list.size();
     assertTrue(b2);
     assertTrue(b3);
+    assertEquals(2, size);
   }
 
   @Test
@@ -28,9 +33,11 @@ public class AvlTreeListTest {
     boolean b4 = list.add(4);
     boolean b5 = list.add(5);
     boolean b6 = list.add(6);
+    int size = list.size();
     assertTrue(b4);
     assertTrue(b5);
     assertTrue(b6);
+    assertEquals(3, size);
   }
 
 }
